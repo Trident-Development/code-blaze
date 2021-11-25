64db89fe8a2f0ed930f30c742c28dc915e375fe7
Allen Tran <allentran08@gmail.com>
2021-11-24 20:33:22 -0800

    checkpoint

---
+++ backend/codes/f4cf2dea-f8e3-4aa8-a279-2eec8cd2e8a9.python
@@ -0,0 +1,7 @@
+def main(nums):
+   for i in nums:
+       print(i)
+   return
+
+
+main([1, 2, 3, 4, 5])
--- temp/temp.java
+++
@@ -1,8 +0,0 @@
-class JavaClass {
-    
-    public static void main(String[] args) {
-        
-        System.out.println("Hello World");
-    }
-    
-}
